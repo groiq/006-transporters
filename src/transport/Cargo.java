b7751f8940713024cef65b1e4c6f7cef8fce6d7a
groiq <35578027+groiq@users.noreply.github.com>
2018-06-19 11:31:39 +0200

    Implement class Cargo

---
+++ src/transport/Cargo.java
@@ -0,0 +1,22 @@
+package transport;
+
+public class Cargo {
+	
+	String label;
+	CargoType state;
+	double weight;
+	
+	Cargo(String label, CargoType state, double weight) {
+		this.label = label;
+		this.state = state;
+		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return "Cargo [label=" + label + ", state=" + state + ", weight=" + weight + "]";
+	}
+	
+	
+
+}
---
+++ src/transport/CargoType.java
@@ -0,0 +1,6 @@
+package transport;
+
+public enum CargoType {
+	liquid, solid
+
+}
